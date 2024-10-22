312cc3b513af0755fb47ff977555818134de6c0f
mari1512 <marimuthu15k@gmail.com>
2024-10-22 19:56:38 +0530

    Order Domain Layer is Done

---
+++ src/services/ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,22 @@
+﻿using Ordering.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        public string Value { get; }
+        private OrderName(string value)=>Value = value;
+
+        public static OrderName Of(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+
+            return new OrderName(value);
+        }
+    }
+}
